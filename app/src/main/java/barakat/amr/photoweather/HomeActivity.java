5b4439994533041f10762a0d2377da6c431a35d1
amrbrkt <amrbrkt@gmail.com>
2017-09-15 18:46:47 +0200

    add base activity

--- app/src/main/java/barakat/amr/photoweather/HomeActivity.java
+++ app/src/main/java/barakat/amr/photoweather/HomeActivity.java
@@ -1,40 +1,21 @@
 package barakat.amr.photoweather;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import barakat.amr.photoweather.base.BaseActivity;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected int getLayoutResource() {
+        return R.layout.activity_home;
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
+    protected int getActivityTitle() {
+        return R.string.app_name;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void afterActivityInflation() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/barakat/amr/photoweather/base/BaseActivity.java
@@ -0,0 +1,48 @@
+package barakat.amr.photoweather.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import barakat.amr.photoweather.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public abstract class BaseActivity extends AppCompatActivity {
+    Activity activity;
+    @Nullable
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResource());
+        activity = this;
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(getActivityTitle());
+        }
+
+        afterActivityInflation();
+    }
+
+    protected abstract
+    @StringRes
+    int getActivityTitle();
+
+    protected abstract
+    @LayoutRes
+    int getLayoutResource();
+
+    protected abstract void afterActivityInflation();
+
+
+}
