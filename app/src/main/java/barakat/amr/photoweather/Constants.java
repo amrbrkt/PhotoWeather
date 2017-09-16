199c2237540394a5657ac3da1e0074c8997e9120
amrbrkt <amrbrkt@gmail.com>
2017-09-16 05:15:45 +0200

    refactor

--- app/src/main/java/barakat/amr/photoweather/Constants.java
+++ app/src/main/java/barakat/amr/photoweather/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
     public static final int CAMERA_REQUEST_CODE = 666;
     public static final int PERMISSION_ACCESS_COARSE_LOCATION = 555;
 
+    //APIs Constants
     public static final String BASE_URL = "https://api.openweathermap.org/data/2.5/";
     public static String APP_ID = "9ef6f7dfbeddbcc60e50407927243b4e";
 }
--- app/src/main/java/barakat/amr/photoweather/imagecapture/ImageCaptureUtils.java
+++ app/src/main/java/barakat/amr/photoweather/imagecapture/ImageCaptureUtils.java
@@ -7,11 +7,10 @@ import android.os.Environment;
 import android.util.Log;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
+
+import barakat.amr.photoweather.DateUtils;
 
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
@@ -49,13 +48,10 @@ public class ImageCaptureUtils {
             }
         }
 
-        // Create a media file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
-                Locale.getDefault()).format(new Date());
         File mediaFile;
         if (type == MEDIA_TYPE_IMAGE) {
             mediaFile = new File(mediaStorageDir.getPath() + File.separator
-                    + timeStamp + ".jpg");
+                    + DateUtils.getTimeStamp() + ".jpg");
         } else {
             return null;
         }
